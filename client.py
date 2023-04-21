b4eaffd92e7037c3271dab28ce2aaa0a32cfb26a
Manna Sandhu <manna@Mannas-MacBook-Pro.local>
2023-04-21 19:05:10 +0530

    first commit

--- client.py
+++ client.py
@@ -16,8 +16,9 @@ canvas1 = None
 
 nameEntry = None
 nameWindow = None
-
-
+gameWindow = None
+ticketGrid = None
+currentNumberList = None
 
 
 
@@ -33,8 +34,35 @@ def saveName():
 
     SERVER.send(playerName.encode())
 
-    
+def createTicket():
+    global gameWindow
+    global ticketGrid
+    mainLabel = Label(gameWindow, width=65, height=16, relief='ridge', borderWidth=5, bg='white')
+    mainLabel.place(x=95, y=119)
 
+    xPos = 185
+    yPos = 130
+    for row in range(0,3):
+        rowList = []
+        for col in range(0,9):
+            if(platform.system() == 'Darwin'):
+                boxButton = Button(gameWindow,
+                font = ("Chalkboard SE", 18),
+                borderwidth=3
+                pady=23,
+                pady=22,
+                bg="#fff176",
+                highlightbackground="#fff176",
+                activebackground='#c5ela5')
+
+                boxButton.place(x=xPos, y=yPos)
+            
+            rowList.append(boxButton)
+            xPos +=64
+        
+        ticketGrid.append(rowList)
+        xPos = 105
+        yPos +=82
 
 
 def askPlayerName():
@@ -70,7 +98,43 @@ def askPlayerName():
     nameWindow.mainloop()
 
 
+def placeNumbers():
+    global ticketGrid
+    global currentNumberList
 
+    for row in range(0,3):
+        randomColList = []
+        counter = 0
+        while counter<=4:
+            randomCol = random.randint(0,8)
+            if(randomCol not in randomColList):
+                randomColList.append(randomCol)
+                counter +=1
+    
+    numberContainer = {
+        "0": [1, 2, 3, 4, 5, 6, 7, 8, 9],
+        "1": [10, 11, 12, 13, 14, 15, 16, 17, 18, 19],
+        "2": [20, 21, 22, 23, 24, 25, 26, 27, 28, 29],
+        "3": [30, 31, 32, 33, 34, 35, 36, 37, 38, 38],
+        "4": [40, 41, 42, 43, 44, 45, 46, 47, 48, 49],
+        "5": [50, 51, 52, 53, 54, 55, 56, 57, 58, 59],
+        "6": [60, 61, 62, 63, 64, 65, 66, 67, 68, 69],
+        "7": [70, 71, 72, 73, 74, 75, 76, 77, 78, 79],
+        "8": [80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90],
+    }
+
+    counter = 0
+    while (counter<len(randomColList)):
+        colNum = randomColList[counter]
+        numbersListByIndex = numberContainer[str(colNum)]
+        randomNumber = random.choice(numbersListByIndex)
+
+        if(randomNumber not in currentNumberList):
+            numberBox = ticketGrid[row][colNum]
+            numberBox.configure(text=randomNumber, fg='black')
+            currentNumberList.append(randomNumber)
+
+            counter+=1
 
 
 def recivedMsg():
